dc605d24c48eef81dd9c64876d2df4e178ae9777
JNCallaGiron <jairocalla3@gmail.com>
2024-10-08 11:15:28 -0300

    Central Configuracion

---
+++ Eureka-SV/src/test/java/com/jairo/Eureka_SV/EurekaSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jairo.Eureka_SV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
